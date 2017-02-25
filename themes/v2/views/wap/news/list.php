582d14c6d5de812ce0d789f3da1fa0674eadb30e
ztenst <754710007@qq.com>
2017-02-25 16:17:02 +0800

    宽度调整

--- themes/v2/views/wap/news/list.php
+++ themes/v2/views/wap/news/list.php
@@ -27,7 +27,7 @@
                     </div>
                     <div class="newsbody">
                         <p class="title ellipsis"><?=$value['title']?></p>
-                        <p class="description"><?=Tools::u8_title_substr($value['desc'],50)?></p>
+                        <p class="description"><?=Tools::u8_title_substr($value['desc'],46)?></p>
                     </div>
                 </a>
             </div>
