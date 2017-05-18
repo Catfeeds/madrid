033455dbf58c1bad6140028efe3dd10349454683
zhangtao <754710007@qq.com>
2017-05-18 10:59:51 +0800

    beian

--- themes/v2/views/home/show/index.php
+++ themes/v2/views/home/show/index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	<title>Tivon的个人主页</title>
 </head>
 <body>
 	<h1>欢迎来到Tivon的主页，稍后会有更多精彩！！</h1>
-
+	<div style="height: 800px"></div>
+	<span>版权所有:Tivon的个人主页 &nbsp;<a href="http://www.miitbeian.gov.cn/" target="_blank">信息产业部备案/许可证编号：苏ICP备16039983号	</a>&nbsp; 
+				<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32041102000005"></span>
 </body>
 </html>
